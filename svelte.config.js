9e481c631439ee725cc6f565543ba3fb1828b40f
Dhiren Audich <dhiren@audich.net>
2023-12-31 01:08:00 -0500

    setup msdvex, sveltePreprocess, and sass

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,15 @@
 import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import sveltePreprocess from 'svelte-preprocess';
+import mdsvex from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: [
+		sveltePreprocess(),
+		mdsvex({
+			extensions: ['.md']
+		})
+	],
 
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
@@ -18,7 +22,9 @@ const config = {
 			precompress: false,
 			strict: true
 		})
-	}
+	},
+
+	extensions: ['.svelte', '.md'],
 };
 
 export default config;
