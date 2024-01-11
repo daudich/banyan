883e67f152e6ed69e9c9b75e4a720cd9935b1001
Dhiren Audich <dhiren@audich.net>
2024-01-10 22:45:13 -0500

    add postcss autoprefixer

--- svelte.config.js
+++ svelte.config.js
@@ -3,11 +3,16 @@ import sveltePreprocess from 'svelte-preprocess';
 import { mdsvex } from 'mdsvex';
 import rehypeSlug from 'rehype-slug';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import autoprefixer from 'autoprefixer';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: [
-		sveltePreprocess(),
+		sveltePreprocess({
+			postcss: {
+				plugins: [autoprefixer]
+			}
+		}),
 		mdsvex({
 			extensions: ['.md'],
 			rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings]
