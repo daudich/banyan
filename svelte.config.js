fb944b25a13eb122ee2a94a0e991990be72a6502
Dhiren Audich <dhiren@audich.net>
2024-01-10 22:42:17 -0500

    add and configure rehpye for heading anchors

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,16 @@
 import adapter from '@sveltejs/adapter-static';
 import sveltePreprocess from 'svelte-preprocess';
 import { mdsvex } from 'mdsvex';
+import rehypeSlug from 'rehype-slug';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: [
 		sveltePreprocess(),
 		mdsvex({
-			extensions: ['.md']
+			extensions: ['.md'],
+			rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings]
 		})
 	],
 
