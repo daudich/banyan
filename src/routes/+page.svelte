43b6068023fed5da62bfca452c6e9f31107717fb
Dhiren Audich <dhiren@audich.net>
2025-03-18 18:21:12 -0400

    add the banyan image to the home screen and place it side by side

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,44 @@
-<div>
-    <h1>Hi, my name is <strong>Banyan</strong></h1>
-    <p>I am a clean modern theme developed for SvelteKit.</p>
-    <p>I was originally developed as a theme for a personal website, but I quickly be adapted for almost any other use case that you can think of.</p>
+<div class="container">
+	<div class="text">
+		<h1>Hi, my name is <strong>Banyan</strong></h1>
+		<p>I am a clean modern theme developed for SvelteKit.</p>
+		<p>
+			I was originally developed as a theme for a personal website, but I can quickly be adapted for
+			almost any other use case that you can think of.
+		</p>
+	</div>
+	<div class="image">
+		<!-- https://www.flickr.com/photos/matthewpaulson/8743231744/in/photostream/ -->
+		<img src="/images/banyan.jpg" alt="A banyan tree with hanging roots by Matthew Paulson." />
+	</div>
 </div>
 
 <style lang="scss">
-    @use '../lib/styles/base/vars';
-    @use '../lib/styles/utils/functions' as _;
+	@use '../lib/styles/base/vars';
+	@use '../lib/styles/utils/functions' as _;
 
-    div {
-        width: 40vw;
-    }
+	// Container styles
+	.container {
+		display: flex;
+		flex-direction: row; // side by side layout
 
-    h1 {
-        font-size: clamp(1.8rem, calc(1rem + 3vw), 3.5rem);
+		// Ensure each child gets 50% of the width
+		> .text,
+		> .image {
+			width: 50%;
+			padding: calc(#{_.size('wholeNote')} * 2);
+		}
+	}
 
-        strong {
-            color: _.palette(vars.$light-theme, 'ink-h1');
-        }
-    }
+	h1 {
+		font-size: clamp(1.8rem, calc(1rem + 3vw), 3.5rem);
 
-    p {
-        font-size: clamp(1.2rem, calc(0.6rem + 2vw), 1.8rem);
-    }
+		strong {
+			color: _.palette(vars.$light-theme, 'ink-h1');
+		}
+	}
+
+	p {
+		font-size: clamp(1.2rem, calc(0.6rem + 2vw), 1.8rem);
+	}
 </style>
