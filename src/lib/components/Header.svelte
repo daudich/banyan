cdcd527e61fba00edac154c13e98dbca2ca6eef0
Dhiren Audich <daudich@users.noreply.github.com>
2025-07-11 12:36:48 -0400

    Created the homepage and footer (#1)
    * organise the main page and styles
    
    * import fonts into styles; add libertine for body
    
    * reorganise the sytlesheets; add styling for typography
    
    * fix SASS functions importation
    
    * add standard sizes to sass; continue to style general layout
    
    * add breakpoints; style the layout
    
    * move header and footer styling into their own components; make dark default colour scheme
    
    * fix padding for layout and header
    
    * abstract brand colours; implement function for breakpoints
    
    * add more styling for header and the front page matter; change the text on the front page
    
    * make light theme false by default; parameterize the functions accordingly
    
    * update sveltekit and vite
    
    * migrated to pnpm; updated packages
    
    * add the banyan image to the home screen and place it side by side
    
    * Add pages for projects and books
    
    * add a slant to the hero image
    
    * wip
    
    * updated packagesa
    
    * started styling footer
    
    * styled the footer headings
    
    * continue to style the footer

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -8,7 +8,13 @@
 				<a href="/blog">Blog</a>
 			</li>
 			<li>
-				<a href="/about">About</a>
+				<a href="/projects">Projects</a>
+			</li>
+			<li>
+				<a href="/books">Books</a>
+			</li>
+			<li>
+				<a href="/about">About Me</a>
 			</li>
 			<li>
 				<a href="/contact">Contact</a>
@@ -16,26 +22,3 @@
 		</ul>
 	</nav>
 </header>
-
-<!-- ... The rest of the file's contents here -->
-<style>
-	header {
-		padding: 1rem;
-		background: lightskyblue;
-		display: flex;
-		flex-wrap: wrap;
-		justify-content: space-between;
-	}
-
-	ul {
-		margin: 0;
-		list-style-type: none;
-		display: flex;
-		gap: 1rem;
-	}
-
-	a {
-		text-decoration: none;
-		color: inherit;
-	}
-</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,46 @@
-<h1>Banyan</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="container">
+	<div class="text">
+		<h1>Hi, my name is <strong>Banyan</strong></h1>
+		<p>I am a clean modern theme developed for SvelteKit.</p>
+		<p>
+			I was originally developed as a theme for a personal website, but I can quickly be adapted for
+			almost any other use case that you can think of.
+		</p>
+	</div>
+	<div class="image">
+		<!-- https://www.flickr.com/photos/matthewpaulson/8743231744/in/photostream/ -->
+		<img src="/images/banyan.jpg" alt="A banyan tree with hanging roots by Matthew Paulson." />
+	</div>
+</div>
+
+<style lang="scss">
+	@use '../lib/styles/base/vars';
+	@use '../lib/styles/utils/functions' as _;
+
+	// Container styles
+	.container {
+		display: flex;
+		flex-direction: row;
+
+		> .text,
+		> .image {
+			width: 50%;
+		}
+
+		.image {
+			clip-path: polygon(20% 0, 100% 0, 100% 100%, 0 100%);
+		}
+	}
+
+	h1 {
+		font-size: clamp(1.8rem, calc(1rem + 3vw), 3.5rem);
+
+		strong {
+			color: _.palette(vars.$light-theme, 'ink-h1');
+		}
+	}
+
+	p {
+		font-size: clamp(1.2rem, calc(0.6rem + 2vw), 1.8rem);
+	}
+</style>
