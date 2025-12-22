eb15e4614e01d174f9d125fc2974425781e21524
Dhiren Audich <dhiren@audich.net>
2025-12-22 14:09:49 -0500

    extract article into its own svelte component; reorganize the styles

---
+++ src/lib/components/Article.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  export let article;
+</script>
+
+<article>
+	<h1>{article.title}</h1>
+	<h2>{article.subtitle}</h2>
+	<h3>Published On: {article.date}</h3>
+
+	<div class="hero">
+		<img src="/images/{article.hero}" alt={article.hero_alt}>
+	</div>
+
+	{#if article.categories.length}
+		<aside>
+			<h2>Posted in:</h2>
+			<ul>
+				{#each article.categories as category}
+					<li>
+						<a href="/blog/category/{category}">
+							{category}
+						</a>
+					</li>
+				{/each}
+			</ul>
+		</aside>
+	{/if}
+
+	<article.Content />
+</article>
--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -1,36 +1,6 @@
-<script>
+<script lang="ts">
+	import Article from '$lib/components/Article.svelte';
 	export let data;
-	const { title, subtitle, date, hero, hero_alt, categories, Content } = data;
 </script>
 
-<svelte:head>
-	<title>My blog - {title}</title>
-	<meta property="og:title" content={title} />
-</svelte:head>
-
-<article>
-	<h1>{title}</h1>
-	<h2>{subtitle}</h2>
-	<h3>Published On: {date}</h3>
-
-	<div class="hero">
-		<img src="/images/{hero}" alt={hero_alt}>
-	</div>
-
-	{#if categories.length}
-		<aside>
-			<h2>Posted in:</h2>
-			<ul>
-				{#each categories as category}
-					<li>
-						<a href="/blog/category/{category}">
-							{category}
-						</a>
-					</li>
-				{/each}
-			</ul>
-		</aside>
-	{/if}
-
-	<Content />
-</article>
+<Article article={data}/>
